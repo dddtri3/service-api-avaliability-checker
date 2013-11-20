2db0297419aac12d990c7aeca18dcb6e3efe3eb8
daniel.shih <shenyushih@gmail.com>
2013-11-20 12:29:58 +0800

    Commit initial copy

---
+++ src/main/java/com/drfirst/mis/service_api_avaliability_checker/App.java
@@ -0,0 +1,15 @@
+package com.drfirst.mis.service_api_avaliability_checker;
+
+import com.dddtri.qa.api.data.AbstractApiData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AbstractApiData test = null;
+    }
+}
